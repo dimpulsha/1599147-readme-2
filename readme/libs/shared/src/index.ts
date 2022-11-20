c557e2b8d9a6df3b00953c60c283ede9977dde68
dimpulsha <dimpulsha@gmail.com>
2022-11-20 12:13:49 +0300

    update lib reexport

--- readme/libs/shared/src/index.ts
+++ readme/libs/shared/src/index.ts
@@ -1,1 +1,5 @@
 export * from './lib/user.interface';
+export * from './lib/comment.interface';
+export * from './lib/post-kind.enum';
+export * from './lib/post-state.enum';
+export * from './lib/post.interface';
