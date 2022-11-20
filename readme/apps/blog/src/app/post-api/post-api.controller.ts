a0d72b87c10f4ab8457d486a5bf0e07b341a8fb0
dimpulsha <dimpulsha@gmail.com>
2022-11-20 20:37:20 +0300

    update post controller

--- readme/apps/blog/src/app/post-api/post-api.controller.ts
+++ readme/apps/blog/src/app/post-api/post-api.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Logger, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Logger, Param, Patch, Post, Query } from '@nestjs/common';
 import { CreatePostDTO } from './dto/create-post.dto';
 import { PostApiService } from './post-api.service';
 const METHOD_NOT_IMPLEMENTED = 'Method not implemented';
@@ -18,8 +18,6 @@ export class PostApiController {
   @Get()
   public async getPostList(@Query() {startCount, endCount, sortKind, sortDirection }) {
     Logger.log('accept request blog/ for post list');
-    // Logger.log(queryKeys);
-    // return (`getPostList() ${JSON.stringify(queryKeys)}`);
     return (`${METHOD_NOT_IMPLEMENTED} ${startCount}, ${endCount}, ${sortKind}, ${sortDirection}`);
   }
 
@@ -45,6 +43,7 @@ export class PostApiController {
   }
 
   @Post('like/:id')
+  @HttpCode(HttpStatus.OK)
   public async likePost(@Param('id') id: string) {
     Logger.log('accept request blog/:id for change like');
     Logger.log(id);
@@ -52,6 +51,7 @@ export class PostApiController {
   }
 
   @Post('repost/:id')
+  @HttpCode(HttpStatus.OK)
   public async repost(@Param('id') id: string) {
     Logger.log('accept request blog/:id for repost');
     Logger.log(id);
