e43540fc4d231bc910ede8fce1050ede76c2201b
dimpulsha <dimpulsha@gmail.com>
2022-11-20 12:27:03 +0300

    add templates for post-api modules

--- readme/apps/blog/src/app/post-api/post-api.module.ts
+++ readme/apps/blog/src/app/post-api/post-api.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { PostApiController } from './post-api.controller';
+import { PostApiService } from './post-api.service';
 
-@Module({})
+@Module({
+  controllers: [PostApiController],
+  providers: [PostApiService],
+})
 export class PostApiModule {}
