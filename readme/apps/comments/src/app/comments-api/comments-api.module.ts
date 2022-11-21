bf3b6377b9b904729562685bc2f45d5d63107a74
dimpulsha <dimpulsha@gmail.com>
2022-11-21 11:17:23 +0300

    recreate comments microservice

--- readme/apps/comments/src/app/comments-api/comments-api.module.ts
+++ readme/apps/comments/src/app/comments-api/comments-api.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { CommentsApiService } from './comments-api.service';
 import { CommentsApiController } from './comments-api.controller';
 
 @Module({
+  providers: [CommentsApiService],
   controllers: [CommentsApiController],
 })
 export class CommentsApiModule {}
