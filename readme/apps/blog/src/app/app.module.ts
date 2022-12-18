a372c95a08b9df31cb3d656a65f0d1914e9ceeaa
dimpulsha <dimpulsha@gmail.com>
2022-12-18 19:30:22 +0300

    add prisma-modules

--- readme/apps/blog/src/app/app.module.ts
+++ readme/apps/blog/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PostApiModule } from './post-api/post-api.module';
 import { PostStorageModule } from './post-storage/post-storage.module';
-
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PostStorageModule,PostApiModule],
+  imports: [PostStorageModule, PostApiModule, PrismaModule],
   controllers: [],
   providers: [],
 })
